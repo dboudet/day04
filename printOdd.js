54dfb88c5ba48edd28b93a6888498558b49b65ce
Dan Boudet <dboudet@palmbeachculture.com>
2021-07-01 14:31:37 -0400

    updates & additions to day 4 challenges

--- printOdd.js
+++ printOdd.js
@@ -1,5 +1,5 @@
 //Print the odd numbers less than 100
-
+// shortcut
 for(i=1; i<=100; i+=2){
     console.log(i)
 }
---
+++ printOddAlt.js
@@ -0,0 +1,5 @@
+//Print the odd numbers less than 100
+// using if
+for(i=0; i<=100; i++){
+    if(i%2===1) console.log(i)
+}
